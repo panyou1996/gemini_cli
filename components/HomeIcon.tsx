e625c45d95e69f022591d61c922a17feaca92cdc
panyou <panyouxg@163.com>
2025-10-14 20:01:59 +0800

    style: use react-icons for HomeIcon for consistency

--- components/HomeIcon.tsx
+++ components/HomeIcon.tsx
@@ -1,21 +1,6 @@
 import React from 'react';
+import { FiHome } from 'react-icons/fi';
 
-const HomeIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    {...props}
-  >
-    <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-    <polyline points="9 22 9 12 15 12 15 22" />
-  </svg>
-);
+const HomeIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => <FiHome {...props} />;
 
 export default HomeIcon;
